b6e82b672fc1f7dd27d536d434c02fc1c658ebf0
M. Athari Anaraki <MosTec1991@users.noreply.github.com>
2025-01-26 16:18:25 +0330

    Make Splash Screen Form

--- src/WebScanner/frmSplashScreen.Designer.cs
+++ src/WebScanner/frmSplashScreen.Designer.cs
@@ -31,11 +31,94 @@ partial class FrmSplashScreen
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "SplashScreen";
+        components = new System.ComponentModel.Container();
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSplashScreen));
+        icoSystenTry = new System.Windows.Forms.NotifyIcon(components);
+        mnuSystemTry = new System.Windows.Forms.ContextMenuStrip(components);
+        settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+        runAtStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+        useLastUsedOptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+        exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+        pbSplashScreen = new System.Windows.Forms.PictureBox();
+        mnuSystemTry.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)pbSplashScreen).BeginInit();
+        SuspendLayout();
+        // 
+        // icoSystenTry
+        // 
+        icoSystenTry.ContextMenuStrip = mnuSystemTry;
+        icoSystenTry.Icon = ((System.Drawing.Icon)resources.GetObject("icoSystenTry.Icon"));
+        icoSystenTry.Text = "TWAIN Scan App";
+        icoSystenTry.Visible = true;
+        // 
+        // mnuSystemTry
+        // 
+        mnuSystemTry.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { settingToolStripMenuItem, exitToolStripMenuItem });
+        mnuSystemTry.Name = "mnuSystemTry";
+        mnuSystemTry.Size = new System.Drawing.Size(112, 48);
+        // 
+        // settingToolStripMenuItem
+        // 
+        settingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { runAtStartupToolStripMenuItem, useLastUsedOptionToolStripMenuItem });
+        settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+        settingToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+        settingToolStripMenuItem.Text = "Setting";
+        // 
+        // runAtStartupToolStripMenuItem
+        // 
+        runAtStartupToolStripMenuItem.Name = "runAtStartupToolStripMenuItem";
+        runAtStartupToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+        runAtStartupToolStripMenuItem.Text = "Run At Startup";
+        runAtStartupToolStripMenuItem.Click += runAtStartupToolStripMenuItem_Click;
+        // 
+        // useLastUsedOptionToolStripMenuItem
+        // 
+        useLastUsedOptionToolStripMenuItem.Name = "useLastUsedOptionToolStripMenuItem";
+        useLastUsedOptionToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+        useLastUsedOptionToolStripMenuItem.Text = "Use Last Used Option";
+        useLastUsedOptionToolStripMenuItem.Click += useLastUsedOptionToolStripMenuItem_Click;
+        // 
+        // exitToolStripMenuItem
+        // 
+        exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+        exitToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+        exitToolStripMenuItem.Text = "Exit";
+        exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+        // 
+        // pbSplashScreen
+        // 
+        pbSplashScreen.Image = ((System.Drawing.Image)resources.GetObject("pbSplashScreen.Image"));
+        pbSplashScreen.Location = new System.Drawing.Point(0, 0);
+        pbSplashScreen.Name = "pbSplashScreen";
+        pbSplashScreen.Size = new System.Drawing.Size(450, 450);
+        pbSplashScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+        pbSplashScreen.TabIndex = 1;
+        pbSplashScreen.TabStop = false;
+        // 
+        // FrmSplashScreen
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(450, 450);
+        Controls.Add(pbSplashScreen);
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        Text = "SplashScreen";
+        Load += FrmSplashScreen_Load;
+        mnuSystemTry.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)pbSplashScreen).EndInit();
+        ResumeLayout(false);
     }
 
+    private System.Windows.Forms.PictureBox pbSplashScreen;
+
+    private System.Windows.Forms.NotifyIcon icoSystenTry;
+
     #endregion
+
+    private ContextMenuStrip mnuSystemTry;
+    private ToolStripMenuItem settingToolStripMenuItem;
+    private ToolStripMenuItem runAtStartupToolStripMenuItem;
+    private ToolStripMenuItem useLastUsedOptionToolStripMenuItem;
+    private ToolStripMenuItem exitToolStripMenuItem;
 }
--- src/WebScanner/frmSplashScreen.cs
+++ src/WebScanner/frmSplashScreen.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using Ardalis.Result;
+using Microsoft.Win32;
 
 namespace WebScanner;
 
@@ -9,7 +10,8 @@ public partial class FrmSplashScreen : Form
     {
         InitializeComponent();
     }
-    
+
+
     public static Result<string> ShowScanForm()
     {
         using var scanForm = new FrmScan();
@@ -28,5 +30,60 @@ public partial class FrmSplashScreen : Form
         var imagesBase64 = imagesResult.Value.Select(image => Convert.ToBase64String(image)).ToList();
 
         return Result.Success(JsonSerializer.Serialize(imagesBase64));
+    }
+
+    private void FrmSplashScreen_Load(object sender, EventArgs e)
+    {
+        runAtStartupToolStripMenuItem.Checked = Properties.Settings.Default.RunAppStartUp;
+        useLastUsedOptionToolStripMenuItem.Checked = Properties.Settings.Default.KeepLastUsedOption;
+        _ = HideSplashScreenAsync();
+        RegisterInStartup(Properties.Settings.Default.RunAppStartUp);
+    }
+    
+    private async Task HideSplashScreenAsync()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(2));
+        this.Hide();
+    }
+
+    private void runAtStartupToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        Properties.Settings.Default.RunAppStartUp = !Properties.Settings.Default.RunAppStartUp;
+        Properties.Settings.Default.Save();
+        runAtStartupToolStripMenuItem.Checked = Properties.Settings.Default.RunAppStartUp;
+        RegisterInStartup(Properties.Settings.Default.RunAppStartUp);
+    }
+
+    private void useLastUsedOptionToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        Properties.Settings.Default.KeepLastUsedOption = !Properties.Settings.Default.KeepLastUsedOption;
+        Properties.Settings.Default.Save();
+        runAtStartupToolStripMenuItem.Checked = Properties.Settings.Default.KeepLastUsedOption;
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+    
+    private void RegisterInStartup(bool isChecked)
+    {
+        var runKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        using RegistryKey? key = Registry.CurrentUser.OpenSubKey(runKey, true);
+        if (key is null)
+        {
+            MessageBox.Show("Unable to open registry to setup startup.", "Error", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            return;
+        }
+
+        if (isChecked)
+        {
+            key.SetValue("TwainScanApp", Application.ExecutablePath);
+        }
+        else
+        {
+            key.DeleteValue("TwainScanApp", false);
+        }
     }
 }
