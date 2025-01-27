ec3d006a4521e3b5ce84e3d8b34b671950d3baec
M. Athari Anaraki <MosTec1991@users.noreply.github.com>
2025-01-27 14:05:03 +0330

    Remove Test

--- src/WebScanner/WebSocketServer.cs
+++ src/WebScanner/WebSocketServer.cs
@@ -15,40 +15,31 @@ internal static class WebSocketServer
         var app = builder.Build();
 
         app.UseWebSockets();
-        
+
         app.Map("/scan", async (HttpContext context) =>
         {
             var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-            var buffer = new byte[10];
-            await webSocket.ReceiveAsync(buffer, CancellationToken.None);
-            var bufferText = System.Text.Encoding.ASCII.GetString(buffer).Trim();
 
-            if (bufferText.Contains("test"))
+            Result<string>? result = null;
+            await Program.StartForm.InvokeAsync(new Action(() => { result = FrmSplashScreen.ShowScanForm(); }));
+
+            if (result is { IsSuccess: true })
             {
-                Result<string>? result = null;
-                await Program.StartForm.InvokeAsync(new Action(() =>
-                {
-                    result = FrmSplashScreen.ShowScanForm();
-                }));
-
-                if (result is { IsSuccess: true })
-                {
-                    await webSocket.SendAsync(
-                        Encoding.ASCII.GetBytes(result.Value),
-                        WebSocketMessageType.Text,
-                        endOfMessage: true,
-                        CancellationToken.None
-                    );
-                }
-                else
-                {
-                    await webSocket.SendAsync(
-                        Encoding.ASCII.GetBytes("Error"),
-                        WebSocketMessageType.Text,
-                        endOfMessage: true,
-                        CancellationToken.None
-                    );
-                }
+                await webSocket.SendAsync(
+                    Encoding.ASCII.GetBytes(result.Value),
+                    WebSocketMessageType.Text,
+                    endOfMessage: true,
+                    CancellationToken.None
+                );
+            }
+            else
+            {
+                await webSocket.SendAsync(
+                    Encoding.ASCII.GetBytes("Error"),
+                    WebSocketMessageType.Text,
+                    endOfMessage: true,
+                    CancellationToken.None
+                );
             }
         });
 
